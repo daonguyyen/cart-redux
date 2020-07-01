5ec03fd48d4b68e8d363e017f2bd7aa3392fd051
daonguyyen <mdnguyen928@gmail.com>
2020-07-01 11:30:16 +0700

    6-PropTypes

--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'; //B1: import connect
 import Products from './../components/Products'
 import Product from '../components/Product';
+import PropTypes from 'prop-types'
 
 class ProductsContainer extends Component {
   render() {
@@ -9,26 +10,42 @@ class ProductsContainer extends Component {
     return (
       //Len store lay state ve sau do truyen state vao products
       <Products>
-         {this.showProducts(products)}
+        {this.showProducts(products)}
       </Products>
     );
   }
 
-  showProducts(products){
+  showProducts(products) {
     var result = null;
-    if(products.length > 0) {
-      result = products.map((product, index)=> {
-        return <Product key = {index} product = {product}/> //Truyen props product de vao component Product lay gia tri thong qua props 
+    if (products.length > 0) {
+      result = products.map((product, index) => {
+        return <Product key={index} product={product} /> //Truyen props product de vao component Product lay gia tri thong qua props 
       })
     }
     return result
   }
 }
 
+//Kiem tra cac props ma component nhan vao: import PropType
+ProductsContainer.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      inventory: PropTypes.number.isRequired,
+      rating: PropTypes.number.isRequired
+    })
+  ).isRequired
+}
+
+
 const mapStateToProps = state => {
   return {
-    products : state.products // B2: products lay tu producer products
+    products: state.products // B2: products lay tu producer products
   }
 }
 
-export default connect(mapStateToProps, null) (ProductsContainer); //B3: thuc hien connect
+export default connect(mapStateToProps, null)(ProductsContainer); //B3: thuc hien connect
