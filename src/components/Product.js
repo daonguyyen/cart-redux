f92350c295237e68032574faec813070fc0c754a
daonguyyen <mdnguyen928@gmail.com>
2020-07-01 15:39:48 +0700

    9- Add to cart p1

--- src/components/Product.js
+++ src/components/Product.js
@@ -30,7 +30,9 @@ class Product extends Component {
             <div className="card-footer">
               <span className="left">{product.price}$</span>
               <span className="right">
-                <a className="btn-floating blue-gradient" data-toggle="tooltip" data-placement="top" title="" data-original-title="Add to Cart" href="#!">
+                <a onClick={() => this.onAddToCart(product)}
+                  className="btn-floating blue-gradient" 
+                  data-toggle="tooltip" data-placement="top" title="" data-original-title="Add to Cart" href="#!">
                   <i className="fa fa-shopping-cart"></i>
                 </a>
               </span>
@@ -39,6 +41,10 @@ class Product extends Component {
         </div>
       </div>
     );
+  }
+
+  onAddToCart = (product) => {
+    this.props.onAddToCart(product)
   }
 
   //Show rating star
--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'; //B1: import connect
 import Products from './../components/Products'
 import Product from '../components/Product';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import { actAddToCart } from './../actions/index'
 
 class ProductsContainer extends Component {
   render() {
@@ -17,9 +18,13 @@ class ProductsContainer extends Component {
 
   showProducts(products) {
     var result = null;
+    var  { onAddToCart } = this.props
     if (products.length > 0) {
       result = products.map((product, index) => {
-        return <Product key={index} product={product} /> //Truyen props product de vao component Product lay gia tri thong qua props 
+        return <Product 
+                    key={index} 
+                    product={product}
+                    onAddToCart = {onAddToCart} /> //Truyen props product de vao component Product lay gia tri thong qua props 
       })
     }
     return result
@@ -48,4 +53,12 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, null)(ProductsContainer); //B3: thuc hien connect
+const mapDispatchToProps = (dispatch, props) => {
+  return {
+    onAddToCart : (product) => {
+      dispatch(actAddToCart(product, 1));
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductsContainer); //B3: thuc hien connect
