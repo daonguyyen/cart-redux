81f4606ed619e986c8cd7a3e75739765efa66b72
daonguyyen <mdnguyen928@gmail.com>
2020-07-01 10:19:40 +0700

    3-show product from data & show rating for each product

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
+//Combine các reducers
 import { combineReducers} from 'redux';
-import product from './product';
+import products from './products';
 
 const appReducers = combineReducers({
-    product
+    products
 });
 
 export default appReducers;
